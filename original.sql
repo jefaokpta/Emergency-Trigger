1fb3ae71723ed6726d0841476455611ac9dea101
Jefferson Reis <jefaokpta@hotmail.com>
2020-03-20 16:20:18 -0300

    otimizando pra situacao atual da discadora

--- original.sql
+++ original.sql
@@ -3,10 +3,10 @@ delimiter //
 BEGIN     
 if new.channel != 'Console/dsp' then
   if substr(new.dcontext,1,6) = 'TRANSF' and new.accountcode >= 2 and new.billsec > 0 then
-    update relcalls set duration=new.duration,billsec=new.billsec,accountcode=new.accountcode,
-      price=(select tarifadorVip(new.billsec,new.accountcode,new.company_id)) 
-        where calldate BETWEEN concat(substr(now(),1,10),' 00:00:00') and now()
-          and channel=new.channel;
+    update relcalls set duration=(duration+new.duration),billsec=(billsec+new.billsec),
+            price=(select tarifadorVip((billsec+new.billsec),accountcode,substr(new.dcontext,8)))
+              where calldate BETWEEN concat(substr(now(),1,10),' 00:00:00') and now()
+                and company_id = substr(new.dcontext,8) and channel=new.channel;
   else
     insert into relcalls (calldate,src,srcfinal,dstfinal,disposition,
       duration,billsec,accountcode,uniqueid,channel,price,userfield,company_id)
